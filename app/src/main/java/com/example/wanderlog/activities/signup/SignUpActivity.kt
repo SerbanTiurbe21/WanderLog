55519e6f70165f8d9fa0d35374466146dc0113de
denard2001 <lazadenard@gmail.com>
2024-01-05 17:51:33 +0200

    -sign up screen is functional -added services: User and Trip

--- app/src/main/java/com/example/wanderlog/activities/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/wanderlog/activities/signup/SignUpActivity.kt
@@ -1,20 +1,26 @@
 package com.example.wanderlog.activities.signup
-
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.wanderlog.MainActivity
 import com.example.wanderlog.R
 import com.example.wanderlog.activities.login.LoginActivity
-import com.example.wanderlog.utils.EmailUtils
-import com.example.wanderlog.utils.PasswordUtils
+import com.example.wanderlog.api.service.UserService
+import com.example.wanderlog.database.models.User
+import com.example.wanderlog.retrofit.RetrofitInstance
+import com.example.wanderlog.utils.EmailUtils.validateEmail
+import com.example.wanderlog.utils.PasswordUtils.validatePassword
 import com.google.android.material.textfield.TextInputEditText
+import retrofit2.Call
+import java.util.Optional
+
 
 class SignUpActivity : AppCompatActivity() {
+
     private lateinit var emailInput: TextInputEditText
     private lateinit var passwordInput: TextInputEditText
     private lateinit var ivInstagram: ImageView
@@ -77,20 +83,50 @@ class SignUpActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            if(!EmailUtils.validateEmail(emailInput.text.toString())){
+            if(!validateEmail(emailInput.text.toString())){
                 Toast.makeText(this, "Invalid email", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
-            if(!PasswordUtils.validatePassword(passwordInput.text.toString())){
+            if(!validatePassword(passwordInput.text.toString())){
                 Toast.makeText(this, "Invalid password", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
-            // TODO: Add user to database
+            val user = User(
+                email = emailInput.text.toString(),
+                password = passwordInput.text.toString(),
+                trips = setOf()
+            )
+            val userService: UserService = RetrofitInstance.getRetrofitInstance().create(
+                UserService::class.java
+            )
+            val call: Call<Optional<User>> = userService.saveUser(user)
+            call.enqueue(object : retrofit2.Callback<Optional<User>> {
+                override fun onResponse(
+                    call: Call<Optional<User>>,
+                    response: retrofit2.Response<Optional<User>>
+                ) {
+                    if (response.isSuccessful) {
+                        val intent = Intent(this@SignUpActivity, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(
+                            this@SignUpActivity,
+                            "Error creating user",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
 
-            var intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+                override fun onFailure(call: Call<Optional<User>>, t: Throwable) {
+                    Toast.makeText(
+                        this@SignUpActivity,
+                        t.message.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
         }
     }
 
--- app/src/main/java/com/example/wanderlog/api/service/UserService.kt
+++ app/src/main/java/com/example/wanderlog/api/service/UserService.kt
@@ -1,4 +1,33 @@
 package com.example.wanderlog.api.service
 
+import com.example.wanderlog.database.dto.LoginRequest
+import com.example.wanderlog.database.models.User
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+import retrofit2.http.Query
+import java.util.Optional
+
 interface UserService {
+    @GET("/api/v1/user/{id}")
+    fun getUserById(@Path("id") id: String): Call<Optional<User>>
+
+    @GET("/api/v1/user/email")
+    fun getUserByEmail(@Query("email") email: String): Call<ResponseBody>
+
+    @POST("/api/v1/user/save")
+    fun saveUser(@Body user: User): Call<Optional<User>>
+
+    @PUT("/api/v1/user/update/{id}")
+    fun updateUserById(@Path("id") id: String, @Body user: User): Call<Optional<User>>
+
+    @PUT("/api/v1/user/update-email")
+    fun updateUserByEmail(@Query("email") email: String, @Body user: User): Call<Optional<User>>
+
+    @POST("/api/v1/auth/login")
+    fun login(@Body loginRequest: LoginRequest): Call<Boolean>
 }
